a99ca2934fb86bf0a3ff0b272c3b7fa50fa0b29a
Harshit Singh <harshit7404046572@gmail.com>
2026-03-13 10:19:27 +0530

    project1

---
+++ practiceProject/project1/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  const data = {
+    message: "Hello from server",
+    method: req.method,
+    url: req.url
+  };
+
+  res.end(JSON.stringify(data));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
